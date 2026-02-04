41bab86e67dee258e3102efc08def9b7116b214b
palixofficial <u.pali2001@gmail.com>
2026-02-04 15:34:01 +0100

    Raw sql file feltoltes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
