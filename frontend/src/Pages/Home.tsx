e47f89a5d2f3d7064e752fc4d7e71585defc935f
palixofficial <u.pali2001@gmail.com>
2026-02-07 14:55:36 +0100

    navbar fix

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,8 +1,5 @@
 const Home = () => {
-    return (
-      <>
-      </>
-    );
-  };
-  
-  export default Home;
+  return <></>;
+};
+
+export default Home;
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,21 +1,28 @@
-import React, { use } from 'react';
-import styles from './Navbar.module.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import styles from "./Navbar.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Services", link: "/services" },
+    { name: "Contact", link: "/contact" },
+  ];
 
-    const navItems = [
-      { name: 'Home', link: '/' },
-      { name: 'About', link: '/about' },
-      { name: 'Services', link: '/services' },
-      { name: 'Contact', link: '/contact' }
-    ];
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    
-  };
-  
-  
-  
-  export default Navbar;
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        {navItems.map((item) => (
+          <li key={item.name}>
+            <button onClick={() => navigate(item.link)}>{item.name}</button>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
