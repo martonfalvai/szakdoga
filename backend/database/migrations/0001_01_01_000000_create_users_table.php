3f0f2254b4c9c309c11275c9785fbbed145071de
palixofficial <u.pali2001@gmail.com>
2026-02-15 18:49:46 +0100

    backend AUTH init. mukodik

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->integer("role")->default(1);
+            $table->string('avatar')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
